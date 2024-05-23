cd05ac68f5f0ff8eec6f42083bf2e91584e67066
Sayan Roy <sayanroy44617@gmail.com>
2024-05-23 12:42:18 +0530

    Section 5 , handling http get request

--- src/main/java/com/tw/controller/UserController.java
+++ src/main/java/com/tw/controller/UserController.java
@@ -2,19 +2,28 @@ package com.tw.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	
-	@GetMapping
-	public String getUser()
+
+	@GetMapping()
+	public String getUsers( @RequestParam(value="page") int page , @RequestParam(value="limit") int limit ,
+			@RequestParam(value="sort" , required = false)String sort   )
 	{
-		return "Getting all the users " ;
+		return "Getting the users  of page no :" + page + " and limit : " + limit + " sort : "+ sort ;
+	}
+	
+	@GetMapping("/{userid}")
+	public String getUser(@PathVariable String userid )
+	{
+		return "Getting the user with id: " + userid;
 	}
 	
 	@PostMapping
